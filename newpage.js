1a4a11b6cc95dad6cfb0e23ee5ce210576dd725f
berishan <58296224+berishan@users.noreply.github.com>
2020-01-05 17:11:34 +0100

    hhh

---
+++ newpage.js
@@ -0,0 +1,56 @@
+function Seen(){
+    var elementi=document.getElementById("p1").innerHTML="What other twist can top this list? No character in Game of Thrones is safe (Yep, I said it earlier). Ninth episodes are known for unexpected twists and turns in Game of Thrones. So, how did we miss this? We had seen some unexpected, surprising and shocking deaths earlier in the twenty-eight episodes. So how could this be better? The answer is one man George RR Martin. It takes extraordinary writing skills to write a twist that will take the viewers by surprise but it takes more to write something that is so unexpected and also makes perfect sense if they rewatch the previous episodes. A wedding turned to a massacre, A pregnant woman stabbed in the belly, A mother of five killed by slitting her throat, A king killed after hearing “The Lannisters send their regards” and to top it all the creepy “Rains of Castamere” playing in the background. If Ned Stark’s beheading caught us off guard then the Red wedding caught us by our balls.";
+    var element=document.getElementById("tv1");
+    element.src="twists/got.bmp";
+}
+function Seen1(){
+    var elementi=document.getElementById("p2").innerHTML="Okay, wow many of you honestly saw this coming? Yep, Frank Underwood is ruthless. Yep, He doesn’t tolerate anyone who is a threat to him. Yep, He has killed someone before. But, this one really caught us off guard. Kate Mara was shown as one of the central characters of the season in the promotions. In the first episode of the second season Frank Underwood asks her to meet him at a railway station. Everything goes well until, out of where Frank pushes his in front of the train. That was the moment when House of Cards earned a reputation as one of the greatest shows of all time, because it not only showed how crazy this show will be, it set the mood right for the entire season 2. This may sound partial but killing of one of the three main character in the first episode of a season requires guts that even Game of Thrones doesn’t have."
+    var element=document.getElementById("tv2");
+    element.src="twists/hoc.bmp"
+}
+function Seen2(){
+    var elementi=document.getElementById("p3").innerHTML="I’m committing a sin here. I’m considering these two mind blowing twists as a single one. Mr.Robot is a computerised version of Fight Club, Agreed. You could’ve seen it coming right from the first episode that Mr Robot was indeed a hallucination and was no one real, Agreed. But the fact that Mr.Robot’s hallucination is just the residual version of the memories of his father was fascinating in itself. To it add another twist that Darlene was actually his sister. Hands down, this was the mind f*cking series I have ever seen.";
+
+    var element=document.getElementById("tv3");
+    element.src="twists/mrr.bmp";
+}
+function Seen3(){
+    var elementi=document.getElementById("p4").innerHTML="Walter White had killed several guys earlier. Most of them were bad or he did it for his defence. But the moment Walter breaks bad is when he lets Jane die. While waking Jesse up, he inadvertently flips Jane onto her back and she starts asphyxiating on her vomit due to an overdose. Walter could’ve saved her. All it could’ve required was for him to wake her up. But he stands there doing nothing. He lets her die because it could help him gain full control over Jesse and also stop Jesse from doing drugs. She was innocent and Jesse was fully in love with him. He lets her die just because she was a threat to his business. This twist in character of Walter not only sends Jesse down a spiral it formed a basement for other cruel, spineless deeds that Walter was going to commit.";
+
+    var element=document.getElementById("tv4");
+    element.src="twists/brba.bmp";
+}
+function Seen4(){
+    var elementi=document.getElementById("p5").innerHTML="The truth is Dexter never really recovers from the fantastic first season. One of the main reasons for that is a brilliant antagonist in the form of Ice Truck killer. We as audience are shown who the Ice Truck Killer is far earlier than Dexter does. So there is not going to be a final twist in store when Dexter meets Rudy. You could not be more wrong. We are in for a roller coaster of twists and turns and the two most important of them being Harry was having an affair with Dexter’s mom and Rudy is Brian Moser who is the elder brother of Dexter Morgan. Yes, you read that right. Dexter had an elder brother who was not shown visions of his traumatic childhood that appeared countless times in the season. We are left as shocked and surprised as Dexter is and this stupendous writing is one of the reasons why Dexter is one of the most celebrated TV shows of this century.";
+    var element=document.getElementById("tv5");
+    element.src="twists/dexter.bmp";
+}
+function Seen5(){
+    var elementi=document.getElementById("p6").innerHTML="Sherlock season 4 aired three years after its previous season. Yet, those three episodes were worth the wait. In the first episode Mary has died. The second episode showed that Sherlock actually had a sister Eurus (Yes, you read that right). Well, but actually those weren’t the biggest twists of the season. The finale saved the best for the last. It was Eurus who was behind Moriarty all the time and Sherlock’s childhood dog “Redbeard” was actually his childhood friend who was killed by Eurus. Sherlock may or may not return for another season but I’d say the splendid writing of the last season has created some insanely unmatchable standards.";
+
+    var element=document.getElementById("tv6");
+    element.src="twists/sherlock.bmp";
+}
+function Seen6(){
+    var elementi=document.getElementById("p7").innerHTML="Homeland might have lost steam right now, but nonetheless it had a brilliant first couple seasons. Damien Lewis is stunning as Nick Brody and it is a fact that the show couldn’t recover without him. In the first season every episode has some new revelation in one form or another and the most important one is when Carrie and Nick go camping and that’s when Nick realises Carrie is suspecting him. Carrie is immediately revealed that the turned PoW is not Nick but his partner Walker is the terrorist. But again it is revealed that they both are in it together and Walker is just attracting the spotlight so that Nick can kill the Vice President in suicide blast, which Nick couldn’t because of the minimal amount of humanity present in him.";
+
+    var element=document.getElementById("tv7");
+    element.src="twists/homeland.bmp";
+} function Seen7(){
+    var elementi=document.getElementById("p8").innerHTML="Long, Long ago when the golden age of television was considered a myth a genius named “David Lynch” decided to make a crime drama. The rest is history. Twin Peaks , though made 25 years ago is one of the most intriguing and fascinating crime dramas ever made in the history of television. Though its plot is filled with a lot of twists and turns none of it comes close to revelation that Leland Palmer is actually been possessed by Killer Bob. So under Bob’s possession Laura is molested, raped and finally killed. This twist was not only shocking but also highly disturbing. ";
+
+    var element=document.getElementById("tv8");
+    element.src="twists/tp.bmp";
+}
+function Seen8(){
+    var elementi=document.getElementById("p9").innerHTML="Lost had a fantastic opening season followed by a not so impressive second season. The season 3 was fantastic. Especially the finale. Throughout the season we had seen flash cuts of Jack not in the island. It was generally assumed that it was from a flashback as Lost had shown us several sequences of the survivors’ life before they have crashed. But in the finale it is being revealed that Jack’s sequence was a fast forward. It was an ending that forever changed the way Lost was going to proceed from there on. The finale left the fans both surprised and perplexed and it is undoubtedly one of the best endings of a season finale ever.";
+
+    var element=document.getElementById("tv9");
+    element.src="twists/lost.bmp";
+}
+function Seen9(){
+    var elementi=document.getElementById("p10").innerHTML="Written by Jonathan Nolan, Westworld was HBO’s magnum opus after Game of Thrones and had high expectations. You may like this how or hate it but you can’t certainly deny the fact that it had a lot of impressive plot twists and deceptions. And when I’m talking about the best twist of the show, I’m not talking about the revelation of Bernard and Arnold being the same person or the death of Ford. I’m talking about the Theresa’s death. Theresa was one of the show’s most complex characters and we were just warming up to watching her and her death by the hands of Bernard(who is also revealed to be a robot) and Ford erasing her from Bernard left us spellbound. It’s after all true that every show has its “Red Wedding” moment.";
+
+    var element=document.getElementById("tv10");
+    element.src="twists/westworld.bmp";
+}
